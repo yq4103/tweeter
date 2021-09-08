99030ec3b1fa7635b17806b5f636681bea28599b
Yirong Qin <yq4103@mun.ca>
2021-09-08 14:23:03 -0400

    css work and appended an array of user tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,66 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1630886204536
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1630972604536
+  }
+];
+
+const createTweetElement = function (dataObj) {
+  const article = `
+  <section class="tweet-container">
+    <div class="user-info">
+      <div class="user-image">
+        <img src="${dataObj.user.avatars}"></img>      
+          <p>${dataObj.user.name}</p>
+          </div>
+        <p>${dataObj.user.handle}</p>
+      </div>
+    <article class="tweet-container-text">${dataObj.content.text}</article>
+    <footer class="footer"><p><time class="timeago">${new Date(dataObj.created_at)}</time></p>
+      <p>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </p>
+    </footer>
+
+  </section>
+  `;
+
+  return article;
+};
+
+
+
+$(document).ready(function() {
+  
+  for (let dataObj of tweetData) {
+    // create an HTML article with the data from the object
+    const $tweet = createTweetElement(dataObj);
+
+    // adding each article to the DOM (#pets-container)
+    $(".tweets").prepend($tweet);
+    
+  }
+});
