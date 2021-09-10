a043d7016db0e642d264c75d864107dffd17b785
Yirong Qin <yq4103@mun.ca>
2021-09-09 22:41:40 -0400

    after code reivew

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,9 @@
-
 $(document).ready(function() {
+  //this function keeps track if the user tries to input more than 140 characters, if yes the number turns red
   $("#tweet-text").on('input', function(event) {
-    //The event is the input event, when a string is inputted into the textbox, an array is created in the browser and the .length gets its lenth.
+    //the event is the input event, when a string is inputted into the textbox, an array is created in the browser and the .length gets its lenth.
     const counter = event.target.value.length;
+    console.log("counter", counter);
     const inputLength = 140 - counter;
     $(".special-counter").text(inputLength);
     if (inputLength < 0) {
