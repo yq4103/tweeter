59afbdeb63c90815c77d26e6170d0154e6b4fbd7
Yirong Qin <yq4103@mun.ca>
2021-09-11 17:48:20 -0400

    word count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,14 +3,13 @@ $(document).ready(function() {
   $("#tweet-text").on('input', function(event) {
     //the event is the input event, when a string is inputted into the textbox, an array is created in the browser and the .length gets its lenth.
     const counter = event.target.value.length;
-    console.log("counter", counter);
     const inputLength = 140 - counter;
     $(".special-counter").text(inputLength);
     if (inputLength < 0) {
       $(".special-counter").addClass('warning-text');
       
     }
-    if (inputLength > 0) {
+    if (inputLength >= 0) {
       $(".special-counter").removeClass('warning-text');
     }
   });
